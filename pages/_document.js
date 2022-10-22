76802fd0b8db5b8c3fbb37b79825bf67a0ff0a63
Deri Kurniawan <deri.netuchi@gmail.com>
2022-10-22 16:14:07 +0700

    Move icons png to /icons

--- pages/_document.js
+++ pages/_document.js
@@ -11,19 +11,19 @@ const Document = () => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/icons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/icons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/icons/favicon-16x16.png"
         />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00f6ff" />
         <link rel="icon" type="image/svg+xml" href="/brand.svg" />
