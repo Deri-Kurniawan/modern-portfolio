00d7fd1aa2c96d05b864fdd227391f5119940911
Deri Kurniawan <deri.netuchi@gmail.com>
2022-11-06 19:57:28 +0700

    Add Next Progressbar Add Next Progressbar for moving page indicator

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import Script from "next/script";
+import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
 import "../styles/customDOM.css";
 import "aos/dist/aos.css";
@@ -6,6 +7,14 @@ import "aos/dist/aos.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress
+        color="#42BFD2"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+        showOnShallow={true}
+        options={{ easing: "ease", speed: 500 }}
+      />
       <Component {...pageProps} />
       <Script src="/scripts/console.log.js" defer></Script>
     </>
